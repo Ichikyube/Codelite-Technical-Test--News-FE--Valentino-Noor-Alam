b4a97f16eba129586f9e6ee7564ed942604968a7
ichikyube <ichikyube@gmail.com>
2023-03-05 23:50:34 +0700

    hmm

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -57,6 +57,63 @@ class NewsController extends Controller
 
     public function update(ArticleStoreRequest $request, $id)
     {
+
+    /**
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\News  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id):JsonResponse
+    {
+        try {
+            $validated = $request->validate([
+                'title' => ['required'],
+                'content' => ['required'],
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->validator->errors()
+            ], 403);
+        }
+
+        try {
+            $post = News::where("id", $id)
+                    ->firstOrFail();
+
+            // trim title and convert it to title case
+            $validated['title'] = Str::of($validated['title'])->trim()->title();
+                
+            if ($request->hasFile('banner')) {
+                $banner = $request->file('banner');
+                $storage = Storage::disk('public');
+
+                if($storage->exists($post->banner))
+                    $storage->delete($post->banner);
+
+                $banner = $request->file('banner');
+                $imageName = date('YmdHis') . "." . $banner->getClientOriginalName();
+                $banner->move(public_path('img'),$imageName);
+                $path =  $request->getSchemeAndHttpHost() ."/img/" . $imageName;
+                $validated['banner'] = $path;
+
+            } else
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Your source is not valid',
+                    'data' => null
+                ], 403);
+            }
+            $post->fill($validated);
+            //$post->update($validated);
+            $post->save();
+            return $this->responseSuccess($post, 'Article Updated Successfully !');
+        }catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
         $payload = $request->only('title','content');
         $file = [
             "banner" => $request->file('banner')
